3f297da9eace4b57a50d077a647b98f372b76edb
WillianFarias <williansousafarias@gmail.com>
2020-12-29 20:16:14 -0300

    estrutura basica do projeto

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace aplicacao_console_c_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
