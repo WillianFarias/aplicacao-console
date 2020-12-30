76c3219cb8e3f7ff1f64515db5e94a54529d4dd6
WillianFarias <williansousafarias@gmail.com>
2020-12-30 15:35:42 -0300

    Menu inicial

--- Program.cs
+++ Program.cs
@@ -5,8 +5,44 @@ namespace revisao_cshap
     class Program
     {
         static void Main(string[] args)
+    {
+      string opcaoUsuario = obterOpcaoUsuario();
+
+      while (opcaoUsuario.ToUpper() != "X")
+      {
+        switch (opcaoUsuario)
         {
-            Console.WriteLine("Hello World!");
+          case "1":
+            //TODO: adicionar aluno
+            break;
+
+          case "2":
+            //TODO: listar alunos
+            break;
+
+          case "3":
+            //TODO: calcular média geral
+            break;
+
+          default:
+            throw new ArgumentOutOfRangeException();
         }
+
+        opcaoUsuario = obterOpcaoUsuario();
+      }
     }
+
+    private static string obterOpcaoUsuario()
+    {
+      Console.WriteLine("Informe a opção desejava: ");
+      Console.WriteLine("1 - Inserir novo aluno: ");
+      Console.WriteLine("2 - Listar alunos: ");
+      Console.WriteLine("3 - Calcular média geral: ");
+      Console.WriteLine("X - Sair");
+      Console.WriteLine();
+
+      string opcaoUsuario = Console.ReadLine();
+      return opcaoUsuario;
+    }
+  }
 }
