56423adc08ab3e088fc59882e185ca057cbaa2bf
WillianFarias <williansousafarias@gmail.com>
2020-12-29 22:40:32 -0300

    criando classe Pilha

--- Program.cs
+++ Program.cs
@@ -7,14 +7,6 @@ namespace aplicacao_console_c_
         static void Main(string[] args)
         {
             //Console.WriteLine("Hello World!");
-
-            int numeroDeVezes = 5;
-
-            for (int i = 0; i < numeroDeVezes; i++)
-            {
-                //interpolation
-                Console.WriteLine($"Bem-vindo ao curso de .NET {i}");
-            }
         }
     }
 }
