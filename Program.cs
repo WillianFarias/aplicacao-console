910e32f9deb5d435b311250106ba1eb58998e4c7
WillianFarias <williansousafarias@gmail.com>
2020-12-29 21:26:18 -0300

    primeiro for com .net

--- Program.cs
+++ Program.cs
@@ -6,7 +6,15 @@ namespace aplicacao_console_c_
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Console.WriteLine("Hello World!");
+
+            int numeroDeVezes = 5;
+
+            for (int i = 0; i < numeroDeVezes; i++)
+            {
+                //interpolation
+                Console.WriteLine($"Bem-vindo ao curso de .NET {i}");
+            }
         }
     }
 }
